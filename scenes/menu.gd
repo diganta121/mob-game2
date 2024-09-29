d94d9db9ca14a88a179a2b32fc89eeb1a6ce991c
dig <70334760+diganta121@users.noreply.github.com>
2024-09-29 16:11:51 +0530

    ee

---
+++ scenes/menu.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_normal_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_arcade_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_slow_pressed() -> void:
+	pass # Replace with function body.
